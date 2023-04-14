dc538a56aaee483ff124d484668fdab61d17a7e0
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-04-14 09:19:11 +0530

    Add files via upload

---
+++ Day5/BankingApplication/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace BankingApplication
+{
+    public class Account
+    {
+        int accno;
+        string name;
+        decimal amount;
+
+        public void accountOpening(int accno, string name, decimal amount)
+        {
+            this.accno = accno;
+            this.name = name;
+            this.amount = amount;
+
+            Console.WriteLine("Account Open Successfully");
+            
+        }
+        public void displayInfo()
+        {
+            
+            Console.WriteLine("Name is : {0}", name);
+            Console.WriteLine("Account Number is: {0}", accno);
+            Console.WriteLine("Initial Amount is :{0}", amount);
+            Console.WriteLine();
+            
+        }
+        public void balance(decimal amt) 
+        {
+            amount += amt;
+            Console.WriteLine(" Current Balance :" + amount);
+            Console.WriteLine();
+         }
+        public void withdraw(decimal wamt) 
+        { 
+            if(amount-wamt< 500)
+            {
+                //Console.WriteLine("Insuffient Balance");
+                throw new ApplicationException("Insufficient Balance");
+                //Console.WriteLine();
+            }
+            else
+            {
+                amount = amount - wamt;
+                Console.WriteLine("After withdrow Current amount:{0}", amount);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Day5/BankingApplication/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a = null;
+            int ch;
+
+            do
+            {
+                ch = 0;
+                Console.WriteLine("**** Banking Application ****");
+
+                Console.WriteLine("Enter your Choice:");
+                Console.WriteLine();
+                Console.WriteLine("1.Account Opening");
+                Console.WriteLine("2.Account Information");
+                Console.WriteLine("3.Deposit an Amount");
+                Console.WriteLine("4.Withdrow an Amount");
+                Console.WriteLine("5.To exit press 0");
+                Console.WriteLine();
+                ch = int.Parse(Console.ReadLine());
+
+
+                switch (ch)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the name");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter the account number");
+                        int accno = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the opening amount:");
+                        decimal amount = decimal.Parse(Console.ReadLine());
+                        a = new Account();
+                        a.accountOpening(accno, name, amount);
+                        break;
+
+                    case 2:
+                        if (a != null)
+                        {
+                            Console.WriteLine("User infromation");
+                            a.displayInfo();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Open An Account First!!!");
+                            Console.WriteLine();
+
+                        }
+                        break;
+                    case 3:
+                        if (a != null)
+                        {
+                            Console.WriteLine("Enter the amount");
+                            decimal amt = decimal.Parse(Console.ReadLine());
+                            a.balance(amt);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Open an Account First!!!");
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 4:
+                        if (a != null)
+                        {
+
+                            Console.WriteLine("Enter the amount to withdraw:");
+                            decimal wamt = decimal.Parse(Console.ReadLine());
+                            a.withdraw(wamt);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Open an account First!!!");
+                            Console.WriteLine();
+                        }
+                        break;
+
+                    case 0:
+                        a = null;
+                        break;
+                    default:
+                        Console.WriteLine("***Wrong choice***");
+                        break;
+
+
+
+                }
+            }
+            while (ch != 0);
+            {
+                Console.WriteLine("Thank you for using banking Application");
+            }
+
+        }
+    }
+}
