91492cb7f534855872b913aa098f136752732aec
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-04-14 09:25:15 +0530

    Add files via upload

---
+++ Day7/AlphabetsAndConsonents/Program.cs
@@ -0,0 +1,43 @@
+﻿
+//Write a program in C# Sharp to count a total number of vowel or consonant in a string.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AlphabetsAndConsonents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int count = 0;
+            int len = 0;
+
+            Console.WriteLine("Enter the String");
+            str = Console.ReadLine();
+            Char[] strr = str.ToCharArray();
+
+            for (int i = 0; i < strr.Length; i++)
+            {
+
+                if (strr[i] == 'A' || strr[i] == 'E' || strr[i] == 'I' || strr[i] == 'O' || strr[i] == 'U' || strr[i] == 'a' || strr[i] == 'e' || strr[i] == 'i' || strr[i] == 'o' || strr[i] == 'u')
+                {
+                    count += 1;
+
+                }
+                else if (strr[i] > 'a' || strr[i] < 'z' || strr[i] > 'A' || strr[i] < 'Z')
+                {
+                    len += 1;
+                }
+            }
+            Console.WriteLine("Number of Vowels: {0}", count);
+            Console.WriteLine("Number of Consonents:{0}", len);
+        }
+    }
+}
+
+
---
+++ Day7/BubleSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubleSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 145, 198, 76, 99, 88, 123 };
+            int temp;
+            Console.WriteLine("Array Elements before Sorting: ");
+            foreach (int i in arr)
+            {
+                Console.Write(i +" ");
+            }
+
+            for (int i = 0; i <= arr.Length-2; i++)
+
+            {
+                for (int j = 0; j <= arr.Length-2; j++)
+                {
+
+
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j+1];
+                        arr[j+1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("\nSorted Array:");
+            foreach(int p in arr)
+            {
+                Console.Write(p +" ");
+            }
+            Console.WriteLine("\n");
+
+        }
+    }
+}
