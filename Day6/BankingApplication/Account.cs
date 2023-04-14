4ac6c50ef5c3bfed8bd2b2229c4787aabb61e79d
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-04-14 09:18:04 +0530

    Add files via upload

---
+++ Day6/BankingApplication/Account.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace BankingApplication 
+{
+    public class Account
+    {
+        int accno;
+        private string _name;
+        public string Name
+        {
+            get { 
+                return _name; 
+            }
+            set
+            {
+                if (value.Length < 5)
+                {
+                    throw new ApplicationException("Name is tooo big U Should Have to change");
+                   
+                }
+                else
+                {
+                   _name = value;
+
+                }
+
+            }
+        }
+       
+        private decimal _amount;
+        public decimal Amount
+        {
+            get
+            {
+                return _amount; 
+            }
+        }
+
+        public void accountOpening(int accno, string name, decimal amount)
+        {
+                 this.accno = accno;
+                _name = name;
+                _amount = amount;
+
+            Console.WriteLine("Account Open Successfully");
+            
+        }
+        public void displayInfo()
+        {
+            
+            Console.WriteLine("Name is : {0}", _name);
+            Console.WriteLine("Account Number is: {0}", accno);
+            Console.WriteLine("Initial Amount is :{0}", _amount);
+            Console.WriteLine();
+            
+        }
+        public void balance(decimal amt) 
+        {
+            _amount += amt;
+            Console.WriteLine(" Current Balance :" + _amount);
+            Console.WriteLine();
+         }
+        public void withdraw(decimal wamt) 
+        { 
+            if(_amount-wamt< 500)
+            {
+                //Console.WriteLine("Insuffient Balance");
+                throw new ApplicationException("Insufficient Balance");
+                //Console.WriteLine();
+            }
+            else
+            {
+                _amount = _amount - wamt;
+                Console.WriteLine("After withdrow Current amount:{0}", _amount);
+                Console.WriteLine();
+            }
+        }
+    }
+}
