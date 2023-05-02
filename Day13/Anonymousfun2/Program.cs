3d51e4afdf575f8b29d82b7b79c66f30071478d2
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-05-02 17:01:03 +0530

    Add files via upload

---
+++ Day13/Anonymousfun2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymousfun2
+{
+    public class Program
+    {
+        public delegate int delemethod(int i,int j);
+        static void Main(string[] args)
+        {
+            int sum;
+           delemethod result= delegate(int i, int j)
+            {
+                return i + j;
+            };
+
+            sum = result(20, 9);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Day13/FileSreamRedaWrite/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSreamRedaWrite
+{
+    class Program
+    {
+        static string file = @"D:\UST_Training\Day13\FileSreamRedaWrite\poem.txt";
+        static void Main()
+        {
+            Write();
+           Read();
+
+        }
+        static void Write()
+        {
+            StreamWriter filestream= new StreamWriter(file);
+            filestream.WriteLine("Example of filestream");
+            filestream.WriteLine("Another example of writing");
+            filestream.Close();
+        }
+        static void Read()
+        {
+            StreamReader fileStream= new StreamReader(file);
+            string s = null;
+            while (s = fileStream.ReadLine() != null)
+            {
+                Console.WriteLine(s);
+          
+        }
+    }
+}
