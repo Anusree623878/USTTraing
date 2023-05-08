60eb67b3aa4b9e7f56297952423ee313d1a9d8b7
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-05-08 09:35:46 +0530

    Add files via upload

---
+++ Day24/DBFIRSTExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFIRSTExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new DBFIRSTEntities())
+            {
+                var std = new Project()
+                {
+                    ProjectId = "P009",
+                    ProjectName = "Gatfes"
+                };
+                context.Projects.Add(std);
+
+                context.SaveChanges();
+            }
+
+            DBFIRSTEntities db=new DBFIRSTEntities();
+
+                    foreach (var project in db.Projects)
+                {
+                    Console.WriteLine(project.ProjectId);
+                    Console.WriteLine(project.ProjectName);
+                }
+                
+            
+        }
+
+
+        
+    }
+}
