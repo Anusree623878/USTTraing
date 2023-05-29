0998eef05970ffec0e2ec75368bf13a5ab5db17b
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-05-29 09:42:11 +0530

    Add files via upload

---
+++ Day42/QSTN2/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSTN2
+{
+
+
+   
+
+    class Result
+    {
+        public static long solve(List<int> arr)
+        {
+            Dictionary<int, int> frequency = new Dictionary<int, int>();
+            long count = 0;
+
+            foreach (int num in arr)
+            {
+                int complement = num;
+                if (frequency.ContainsKey(complement))
+                    count += frequency[complement];
+
+                if (frequency.ContainsKey(num))
+                    frequency[num]++;
+                else
+                    frequency[num] = 1;
+            }
+
+            return count;
+        }
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+            long result = Result.solve(arr);
+            textWriter.WriteLine(result);
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+
+
+}
---
+++ Day42/QSTN3/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSTN3
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    class Result
+    {
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            int maxHeight = candles.Max();
+            int count = 0;
+
+            foreach (int candle in candles)
+            {
+                if (candle == maxHeight)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+            int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+            List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+            int result = Result.birthdayCakeCandles(candles);
+            textWriter.WriteLine(result);
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
