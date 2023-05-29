0cd4f2771cccfc6342b551415032a15613334e0b
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-05-29 09:43:09 +0530

    Add files via upload

---
+++ Day43/Anusree_coding_27-05-23/QSTN1/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSTN1
+{
+    
+
+    class Solution
+    {
+        static void plusMinus(int[] arr)
+        {
+            int n = arr.Length;
+            float positiveCount = 0;
+            float negativeCount = 0;
+            float zeroCount = 0;
+
+            foreach (int num in arr)
+            {
+                if (num > 0)
+                    positiveCount++;
+                else if (num < 0)
+                    negativeCount++;
+                else
+                    zeroCount++;
+            }
+
+            float positiveRatio = positiveCount / n;
+            float negativeRatio = negativeCount / n;
+            float zeroRatio = zeroCount / n;
+
+            Console.WriteLine(positiveRatio.ToString("F6"));
+            Console.WriteLine(negativeRatio.ToString("F6"));
+            Console.WriteLine(zeroRatio.ToString("F6"));
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            plusMinus(arr);
+        }
+    }
+
+}
