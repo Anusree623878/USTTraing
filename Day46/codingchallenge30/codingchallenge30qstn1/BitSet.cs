ac419c3431dedd785f8b8718601aee86338e899a
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-06-02 09:40:08 +0530

    Add files via upload

---
+++ Day46/codingchallenge30/codingchallenge30qstn1/BitSet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingchallenge30qstn1
+{
+    internal class BitSet
+    { 
+            private BitArray bits;
+
+            public BitSet(int size)
+            {
+                bits = new BitArray(size);
+            }
+
+            public void And(BitSet other)
+            {
+                bits.And(other.bits);
+            }
+
+            public void Or(BitSet other)
+            {
+                bits.Or(other.bits);
+            }
+
+            public void Xor(BitSet other)
+            {
+                bits.Xor(other.bits);
+            }
+
+            public void Flip(int index)
+            {
+                bits.Set(index, !bits.Get(index));
+            }
+
+            public void Set(int index)
+            {
+                bits.Set(index, true);
+            }
+
+            public int Cardinality()
+            {
+                int count = 0;
+                for (int i = 0; i < bits.Count; i++)
+                {
+                    if (bits.Get(i))
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+    
+}
---
+++ Day46/codingchallenge30/codingchallenge30qstn1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingchallenge30qstn1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            int n = int.Parse(inputs[0]);
+            int m = int.Parse(inputs[1]);
+
+            BitSet bs1 = new BitSet(n);
+            BitSet bs2 = new BitSet(n);
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] operation = Console.ReadLine().Split(' ');
+                string op = operation[0];
+                int set = int.Parse(operation[1]);
+                int index = int.Parse(operation[2]);
+
+                if (op == "AND")
+                {
+                    if (set == 1)
+                    {
+                        bs1.And(bs2);
+                    }
+                    else
+                    {
+                        bs2.And(bs1);
+                    }
+                }
+                else if (op == "OR")
+                {
+                    if (set == 1)
+                    {
+                        bs1.Or(bs2);
+                    }
+                    else
+                    {
+                        bs2.Or(bs1);
+                    }
+                }
+                else if (op == "XOR")
+                {
+                    if (set == 1)
+                    {
+                        bs1.Xor(bs2);
+                    }
+                    else
+                    {
+                        bs2.Xor(bs1);
+                    }
+                }
+                else if (op == "FLIP")
+                {
+                    if (set == 1)
+                    {
+                        bs1.Flip(index);
+                    }
+                    else
+                    {
+                        bs2.Flip(index);
+                    }
+                }
+                else if (op == "SET")
+                {
+                    if (set == 1)
+                    {
+                        bs1.Set(index);
+                    }
+                    else
+                    {
+                        bs2.Set(index);
+                    }
+                }
+
+                Console.WriteLine($"{bs1.Cardinality()} {bs2.Cardinality()}");
+            }
+        }
+    }
+
+
+}
+
