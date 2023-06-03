cac0c50df28ae919d9ea64be7620f576af63a618
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-06-03 17:54:41 +0530

    Add files via upload

---
+++ Day48/ProductMicroService/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using ProductMicroService.Models;
+
+using System.Configuration;
+
+using ProductMicroservice.Repository;
+using ProductMicroService.Repository;
+using ProductMicroService.DBContexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ProductContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("ProductDB")));
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Day48/ProductMicroService/Repository/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using ProductMicroService.Models;
+using System.Collections.Generic;
+
+namespace ProductMicroService.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+
+        Product GetProductById(int ProductId);
+
+        void InsertProduct(Product Product);
+
+        void DeleteProduct(int  ProductId);
+
+        void UpdateProduct(Product Product);
+
+        void Save();
+    }
+}
