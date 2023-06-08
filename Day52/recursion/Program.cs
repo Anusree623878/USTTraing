aa4ff67f9ed8433cc103934c5949188ef2078be0
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-06-08 16:47:34 +0530

    Add files via upload

---
+++ Day52/recursion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursion
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Enter a Number");
+
+            //read number from user
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //invoke the static method
+
+            double factorial = Factorial(number);
+
+            //print the factorial result
+
+            Console.WriteLine("factorial of" + number + "=" + factorial.ToString());
+
+        }
+
+        public static double Factorial(int number)
+
+        {
+
+            if (number == 0)
+
+                return 1;
+
+            return number * Factorial(number - 1);//Recursive call
+
+        }
+
+    }
+
+}
