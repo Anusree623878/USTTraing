611cc44e38e42165bac8a71128f3671dd8ff2466
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-06-21 10:08:18 +0530

    Add files via upload

---
+++ Day64/SchoolProject/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Data;
+using SchoolProject.Model;
+
+namespace SchoolProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StudentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Student student)
+        {
+            _db.Add(student);
+            _db.SaveChanges();
+            return Ok(student);
+        }
+
+        //[HttpGet]
+
+
+
+
+
+    
+    }
+
+}
---
+++ Day64/SchoolProject/Model/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolProject.Model
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required] 
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        [Range(1000000000, 9999999999, ErrorMessage = "Invalid phone number.")]
+        public int PhoneNumber { get; set; }
+    }
+}
