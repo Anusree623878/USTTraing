b22910bc13d60d5f0c3f02addb3a59c463e4a27d
Anusree623878 <129934872+Anusree623878@users.noreply.github.com>
2023-06-21 09:58:26 +0530

    Add files via upload

---
+++ Day61/webapi/webapiwithcrud/Controllers/productController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapiwithcrud.Connection;
+using webapiwithcrud.Model;
+
+namespace webapiwithcrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+        public class ProductssController : ControllerBase
+
+        {
+
+            private readonly DbContextdemo _db;
+
+            public ProductssController(DbContextdemo db)
+
+            {
+
+                _db = db;
+
+            }
+
+            [HttpPost]
+
+            public ActionResult<DbContextdemo> Create([FromBody] DbContextdemo productList)
+
+            {
+
+                _db.DbContextdemos.Add(productList);
+
+                _db.SaveChanges();
+
+                return Ok(productList);
+
+            }
+
+            [HttpGet]
+
+            public ActionResult<DbContextdemo> GetAll()
+
+            {
+
+                return Ok(_db.DbContextdemos.ToList());
+
+            }
+
+            [HttpGet("{id:int}")]
+
+            public ActionResult<DbContextdemo> Get(int id)
+
+            {
+
+                var result = _db.DbContextdemos.Find(id);
+
+                return Ok(result);
+
+            }
+
+            [HttpPut("{id:int}")]
+
+            public ActionResult<DbContextdemo> Put([FromBody] DbContextdemo pro)
+
+            {
+
+                _db.DbContextdemos.Update(pro);
+
+                _db.SaveChanges();
+
+                return Ok(pro);
+
+            }
+
+            [HttpDelete("{id:int}")]
+
+            public ActionResult<DbContextdemo> Delect(int id)
+
+            {
+
+                var result = _db.DbContextdemos.Find(id);
+
+                 _db.DbContextdemos.Remove(result);
+
+                _db.SaveChanges();
+
+                return Ok();
+
+            }
+
+        }
+
+}
+
+
---
+++ Day61/webapi/webapiwithcrud/Model/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace webapiwithcrud.Model
+{
+    public class Product
+    {
+        public int productId;
+        public string? productName;
+        public string? description;
+    }
+}
